f7c7e3d5e024711acb81d14a052698c6cbf673a6
junguk03 <97594871+junguk03@users.noreply.github.com>
2023-05-17 13:47:55 +0900

    Add files via upload

---
+++ 4_1_number.py
@@ -0,0 +1,18 @@
+print(-1)
+print(0)
+print(1) # int
+print(1.1) # float
+print('1+1',1+1)
+print('1-2',1-2)
+print('2*2',2*2)
+print('4/2',4/2)
+print('5//2',5//2)
+print('5%2',5%2)
+print('5/2',5/2)
+
+import math
+print('math.sqrt(4)',math.sqrt(4)) #sqrt(a)는 a의 제곱근
+print('math.pow(4,2)',math.pow(4,2)) #pow(a,b)는 a^b
+
+import random
+print('random.random()',random.random())
---
+++ 4_3_list.py
@@ -0,0 +1,12 @@
+students = ["egoing", "sori", "maru"]
+grades = [2, 1, 4]
+print("students[2]",students[2])
+print("len(students)",len(students))
+print("max(grades)",max(grades))
+print("sum(grades)",sum(grades))
+
+import statistics
+print("statistics.mean(grades)",statistics.mean(grades))
+
+import random
+print("random.choice(students)",random.choice(students))
